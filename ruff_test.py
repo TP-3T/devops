817878069acbad3f15d8468f72ce61b49630e741
Phyo Thu Kha <phyotkha@outlook.com>
2025-09-25 15:05:07 -0700

    Added a badly written Python code to test Ruff github actions

---
+++ ruff_test.py
@@ -0,0 +1,16 @@
+import os, sys;
+
+def BADfunctionName():
+     a =  1
+     b=2
+     c =3;d=4;
+     result =a+b+c+d
+     print(   "The result is: "+str(result))
+     if(result>5):print("Greater than 5")
+     else:print('Less or equal to 5')
+
+def unused_function():
+    x = 42
+    y = x * 2
+
+BADfunctionName()
