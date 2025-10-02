4228463e7016f293c5c0e28a4da1ce4063360bc7
Phyo Thu Kha <phyotkha@outlook.com>
2025-10-02 14:43:51 -0700

    Added a cs code without docstring

---
+++ bad-csharp-code.cs
@@ -0,0 +1,15 @@
+public class Program
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    static void Main(string[] args)
+    {
+        Program prg = new Program();
+        int number = prg.Add(1, 3);
+
+        Console.WriteLine(number);
+    }
+}
